0ca946e0d9d2fbc342b2b54446115133de7e2287
Xaba73 <wowforxab@mail.ru>
2022-10-24 15:15:34 +0300

    chore: сделал фильтр по годам

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,20 +1,29 @@
-import React from "react";
+import React, {useState} from "react";
 
-import './Expenses.css';
-import ExpensiveItem from './ExpenseItem';
-import Card from '../UI/Card';
+import "./Expenses.css";
+import ExpensiveItem from "./ExpenseItem";
+import Card from "../UI/Card";
+import ExpensesFilter from "./ExpensesFilter";
+
 
 const Expenses = (props) => {
-    return (
-        <Card className='expenses'>
-            {props.items.map((item) => (
-                <ExpensiveItem 
-                title={item.title}
-                amount={item.amount}
-                date={item.date}
-                />)
-            )}
-        </Card>
-    );
-}
+  const [enterdYear, setEnteredYead] = useState("2021");
+
+  const filterChangeHandler = selectedYear => {
+   setEnteredYead(selectedYear);
+  }
+
+  return (
+      <Card className="expenses">
+        <ExpensesFilter selected={enterdYear} onChangeFilter = {filterChangeHandler}></ExpensesFilter>
+        {props.items.map((item) => (
+          <ExpensiveItem
+            title={item.title}
+            amount={item.amount}
+            date={item.date}
+          />
+        ))}
+      </Card>
+  );
+};
 export default Expenses;
